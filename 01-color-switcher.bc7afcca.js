2ca7542b205121e70b661de501561f264f6a79e6
enquire07 <145021641+enquire07@users.noreply.github.com>
2024-04-17 10:36:03 +0000

    Deploying to gh-pages from @ enquire07/goit-js-hw-09@09bfd97af0ff9fa0e308dc242a064666494c75ac 🚀

--- 01-color-switcher.bc7afcca.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");let o=null;n.disabled=!0,e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),n.addEventListener("click",(function(){clearInterval(o),n.disabled=!0,e.disabled=!1}));
-//# sourceMappingURL=01-color-switcher.bc7afcca.js.map
